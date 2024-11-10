e432bc3e13a1c039344de7381c5a6a5fad38e90e
Syeda130 <syedazohra130@gmail.com>
2024-11-10 21:27:28 +0530

    mongodb connection, auth & admin controller - code for authorization

--- backend/src/index.js
+++ backend/src/index.js
@@ -6,11 +6,28 @@ import adminRoutes from "./routes/admin.route.js"
 import songRoutes from "./routes/song.route.js"
 import albumRoutes from "./routes/album.route.js"
 import statRoutes from "./routes/stat.route.js"
+import { connectDB } from "./lib/db.js";
+import { clerkMiddleware } from '@clerk/express';
+import fileUpload from "express-fileupload";
+import path from "path";
 
 dotenv.config();
 
+const __dirname = path.resolve();
 const app = express();
 const PORT = process.env.PORT;
+
+app.use(express.json()); // to parse req.body
+app.use(clerkMiddleware()); // this will add auth to req obf(to identify which user had loged in)
+app.use(fileUpload({
+    useTempFiles: true,
+    tempFileDir: path.join(__dirname, "tmp"),
+    createParentPath: true,
+    limits: {
+        fileSize: 10 * 1024 * 1024, // 10MB  max file size
+    },
+}));
+
 
 app.use("/api/users", userRoutes);
 app.use("/api/auth", authRoutes);
@@ -19,8 +36,14 @@ app.use("/api/songs", songRoutes);
 app.use("/api/albums", albumRoutes);
 app.use("/api/stats", statRoutes);
 
+//error handler 
+app.use((err,req,re,next) => {
+    res.status(500).json({message: process.env.NODE_ENV === "production" ? "Internal server error" : err.message});
+})
+
 app.listen(PORT, () => {
     console.log("server is running on port " + PORT);
+    connectDB();
 });
 
  
--- backend/src/routes/admin.route.js
+++ backend/src/routes/admin.route.js
@@ -1,9 +1,16 @@
 import { Router } from "express";
+import { createsong, deleteSong, createAlbum, deleteAlbum, checkAdmin } from "../controller/admin.controller.js";
+import { protectRoute, requireAdmin } from "../middleware/auth.middleware.js";
 
 const router = Router();
 
-router.get('/', (req,res) => {
-    res.send("admin route with GET method");
-})
+router.use(protectRoute, requireAdmin);
+
+router.get("/check", checkAdmin);
+
+router.post('/songs', createsong);
+router.delete("/songs/:id", deleteSong);
+router.post("/albums", createAlbum);
+router.delete("/albums/:id", deleteAlbum);
 
 export default router
